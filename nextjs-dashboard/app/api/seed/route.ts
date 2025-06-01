a44746409c8887811f35abb943791414c3084133
Aneetta Jijo <aneettajijop@gmail.com>
2025-06-01 12:37:34 -0400

    dependencies

--- nextjs-dashboard/app/api/seed/route.ts
+++ nextjs-dashboard/app/api/seed/route.ts
@@ -1,6 +1,8 @@
-import bcrypt from 'bcrypt';
+'use server'
+
+import bcrypt from 'bcryptjs';
 import postgres from 'postgres';
-import { invoices, customers, revenue, users } from '../lib/placeholder-data';
+import { invoices, customers, revenue, users } from '../../lib/placeholder-data';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
